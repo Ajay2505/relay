a917935a23a4520e4ae7fb57b319b93032d1a586
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-07-26 22:36:04 +0530

    sa

--- global/global.js
+++ global/global.js
@@ -8,64 +8,82 @@ const inf = () => {
     map();
 }
 
+// const mapInfo = [
+//     {
+//         lang: "Telugu",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Hindi",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "English",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Marathi",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Tamil",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Kannada",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Malayalam",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Bengali",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Assamese",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Oriya",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Bhojpuri",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Gujarati",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Punjabi",
+//         info: "Something about it"
+//     },
+//     {
+//         lang: "Bihari",
+//         info: "Something about it"
+//     },
+// ]
+
 const mapInfo = [
     {
-        lang: "Telugu",
-        info: "Something about it"
+        lang: "English"
     },
     {
-        lang: "Hindi",
-        info: "Something about it"
+        lang: "French"
+    }, 
+    {
+        lang: "German",
     },
     {
-        lang: "English",
-        info: "Something about it"
+        lang: "Spanish",
     },
     {
-        lang: "Marathi",
-        info: "Something about it"
-    },
-    {
-        lang: "Tamil",
-        info: "Something about it"
-    },
-    {
-        lang: "Kannada",
-        info: "Something about it"
-    },
-    {
-        lang: "Malayalam",
-        info: "Something about it"
-    },
-    {
-        lang: "Bengali",
-        info: "Something about it"
-    },
-    {
-        lang: "Assamese",
-        info: "Something about it"
-    },
-    {
-        lang: "Oriya",
-        info: "Something about it"
-    },
-    {
-        lang: "Bhojpuri",
-        info: "Something about it"
-    },
-    {
-        lang: "Gujarati",
-        info: "Something about it"
-    },
-    {
-        lang: "Punjabi",
-        info: "Something about it"
-    },
-    {
-        lang: "Bihari",
-        info: "Something about it"
-    },
-]
+        lang: "Indian Languages"
+    }
+];
 
 const calc = () => {
     document.querySelector(".input_track").addEventListener("input", (evt) => {
@@ -161,26 +179,72 @@ const map = () => {
       document.querySelector(".list_div").classList.remove("show");
     });
     
-    mapInfo.forEach(info => {
+    mapInfo.forEach((info, idx) => {
         const button = document.createElement("button");
         button.textContent = info?.lang;
+        if (idx === 0) {
+            button.classList.add("focus");
+        }
         document.querySelector(".list_div").appendChild(button);
         button.addEventListener("click", (evt) => {
             document.querySelector(".map_input").value = evt.target.innerText;
         });
     });
 
+    let focusIndex = 0;
+
     document.querySelector(".map_input").addEventListener("input", (evt) => {
         const value = evt.target.value;
+        focusIndex = 0;
+        if (value.length > 0) {
+            document.querySelector(".x_mark").classList.add("show");
+            document.querySelector(".map_icon").classList.add("hide");
+        } else {
+            document.querySelector(".x_mark").classList.remove("show");
+            document.querySelector(".map_icon").classList.remove("hide");
+        }
         mapInfo.forEach((info, idx) => {
             if (info?.lang.toLocaleLowerCase().match(value.toLocaleLowerCase())) {
-                document.querySelectorAll(".list_div button")[idx].classList.remove("hide")
+                document.querySelectorAll(".list_div button")[idx].classList.remove("hide");
             } else {
-                document.querySelectorAll(".list_div button")[idx].classList.add("hide")
+                document.querySelectorAll(".list_div button")[idx].classList.add("hide");
             }
+            document.querySelectorAll(".list_div button")[idx].classList.remove("focus");
+            document.querySelector(".list_div button:not(.hide)")?.classList.add("focus");
         });
     });
+    
 
+    document.querySelector(".map_input").addEventListener("keydown", (evt) => {
+        if (evt.key === "Enter") {
+            document.querySelector(".list_div button.focus")?.click();
+            evt.target.blur();
+        } else if(evt.key === "ArrowUp") {
+            if (focusIndex <= 0) {
+                console.log("at top");
+                return;
+            } else {
+                focusIndex--;
+                console.log("go up");
+            }
+        } else if(evt.key === "ArrowDown") {
+            if (focusIndex + 1 > document.querySelectorAll(".list_div button:not(.hide)")?.length) {
+                console.log("end");
+                console.log(focusIndex);
+            } else {
+                console.log("go down");
+                focusIndex++;
+                console.log(focusIndex);
+            }
+        }
+
+    });
+
+    document.querySelector(".x_mark").addEventListener("click", () => {
+        document.querySelector(".map_input").value = "";
+        const event = new Event('input', { bubbles: true });
+        document.querySelector(".map_input").dispatchEvent(event);
+    });
 }
 
 document.querySelector(".footer_input").addEventListener("focus", (evt) => {
