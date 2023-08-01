e8595d058a40fc7a325a69ba0c681eb93c1def24
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-08-01 17:56:44 +0530

    sa

--- pages/index.js
+++ pages/index.js
@@ -244,4 +244,5 @@ if(navigator.platform.match('Mac') !== null) {
     document.querySelector('.vector_section').style.background = '#8E5FE7'; //  for Mac users
 } else {
     document.querySelector('.vector_section').style.background = '#8355E4'; // for Windows users
+    document.querySelector('.curve').style.background = '#8355E4'; 
 }
